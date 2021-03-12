463c470601037f3025fee12fc65e8e32a10c26ce
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-03-12 18:31:46 +0300

    HW1.py
    List exercise with python

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,33 @@
-#Explain your work
+#Tuğçe Nur Ergen
+#List application: hw1
 
-#Question 1
-for x in range(a):
-	print(a)
+list1 = []  #odd numbers
+list2 = []  #even numbers
+
+for i in range(1,10):
+    num = int(input("Please enter a number: "))
+
+    if (num%2==0): #this list will keep even numbers
+        list2.append(num)
+        
+    elif (num%2!= 0): #this list will keep odd numbers
+        list1.append(num)
+
+print("1. Odd numbers:"+ str(list1))
+print("2. Even numbers:" +str(list2))
+
+list1.extend(list2)
+print("Extended list:" + str(list1)
+      )  #main list is list1
+
+for i in range(0,9):
+    list1[i]=list1[i]*2
+list1.sort()
+print("Multiplied each value in our list by 2 and sorted \n"+ str(list1))
+
+print("\n data types of the elements in the list:\n")
+for i in range(0,9):
+    print(type(list1[i]))
+    
+
+ 
