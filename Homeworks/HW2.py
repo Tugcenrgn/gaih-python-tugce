ba492f76ffee5dc8905123bf9d339c07644ae3b6
tuğçe nur ergen <60977455+Tugcenrgn@users.noreply.github.com>
2021-03-14 12:46:13 +0300

    HW2

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,57 @@
-#Explain your work
+CV={
+    "cv1":{
+    'Name':"tuğçe nur",
+    'Surname':"Ergen",
+    'e-mail':"tugcenurergen9@gmail.com",
+    'Birthyear': 2001,
+    'Adres':"Bursa",
+    'Job':"Computer Engineer",
+    'phone': "05436785467"
+},
 
-#Question 1
-for x in range(a):
-	print(a)
+"cv2":{
+    'Name':"orçun",
+    'Surname':"Ergen",
+    'e-mail':"orerergen03@gmail.com",
+    'Birthyear': 2005,
+    'Adres':"Kocaeli",
+    'Job':"Electrician",
+    'phone': "05436778967"
+},
+
+"cv3":{
+    'Name':"Halis",
+    'Surname':"Oy",
+    'e-mail':"hto3001@gmail.com",
+    'Birthyear': 2000,
+    'Adres':"İstanbul",
+    'Job':"Computer programmer",
+    'phone': "05413756987"
+},
+
+"cv4":{
+    'Name':"Melek",
+    'Surname':"üreni",
+    'e-mail':"melreni56@gmail.com",
+    'Birthyear': 2001,
+    'Adres':"İstanbul",
+    'Job':"Doctor",
+    'phone': "05678968590"
+},
+
+"cv5":{
+    'Name':"Yüksel",
+    'Surname':"Ergen",
+    'e-mail':"yuko1903@gmail.com",
+    'Birthyear': 1971,
+    'Adres':"Ordu",
+    'Job':"accountant",
+    'phone': "05682341235"
+}
+}
+print(CV['cv1'])
+print(CV['cv2'])
+print(CV['cv3'])
+print(CV['cv4'])
+print(CV['cv5'])
+
